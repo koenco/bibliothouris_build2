2c1bd115fa8fc46d18a4fed3a1065964a9818f83
koenc <koenc@ubuntu.(none)>
2012-09-27 11:24:00 +0200

    fixed reset_modules script

--- reset_custom_modules.sh
+++ reset_custom_modules.sh
@@ -1,18 +1,26 @@
-cd /var/www/bibliothouris_build/sites/all/modules
+cd /var/www/bibliothouris_build/sites/all/modules/
 rm -rf custom
+
+mkdir custom
+cd custom
 
 git init
 git remote add origin https://github.com/koenco/bibliothouris_modules.git
 git clone https://github.com/koenco/bibliothouris_modules.git
 
-mv bibliothouris_modules custom
+cp -R bibliothouris_modules/* .
+rm -rf bibliothouris_modules
 
-cd /var/www/bibliothouris_build/sites/all/themes
+cd /var/www/bibliothouris_build/sites/all/themes/
 
 rm -rf custom
+
+mkdir custom
+cd custom
 
 git init
 git remote add origin https://github.com/koenco/bibliothouris_themes.git
 git clone https://github.com/koenco/bibliothouris_themes.git
 
-mv bibliothouris_themes custom
+cp -R bibliothouris_themes/* .
+rm -rf bibliothouris_themes
